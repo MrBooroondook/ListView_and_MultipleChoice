3d46115cb5426f1a2a91f088ab236d1faabc0edd
Mr. Booroondook <Mr.Booroondook@gmail.com>
2020-04-16 00:02:59 +0400

    Add ListView_MultipleChoice

--- app/src/main/java/mr/booroondook/listview_and_multiplechoice/MainActivity.java
+++ app/src/main/java/mr/booroondook/listview_and_multiplechoice/MainActivity.java
@@ -3,6 +3,8 @@ package mr.booroondook.listview_and_multiplechoice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] lastNames = getResources().getStringArray(R.array.last_names);
+        ListView listView = findViewById(R.id.list_view);
+
+        listView.setAdapter(new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_multiple_choice,
+                lastNames));
     }
 }
